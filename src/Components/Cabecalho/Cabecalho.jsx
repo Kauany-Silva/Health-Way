e68977df02ade455f7c22e0bf591edc8e14f890a
Kauany-Silva <kauany.moraes@etec.sp.gov.br>
2025-09-19 22:25:30 -0300

    Update Cabecalho.jsx
    Atualização do cabeçalho

--- src/Components/Cabecalho/Cabecalho.jsx
+++ src/Components/Cabecalho/Cabecalho.jsx
@@ -1,15 +1,13 @@
 import style from './Cabecalho.module.css';
-import Logo from '../../Assets/Imagens/Logo.png'
-
+import Logo from '../../Assets/Imagens/Logo.png';
 
 const Cabecalho = ({ titulo }) => {
-    
-    return (
-        <header className={style.Cabecalho}>
-            <img src={Logo} alt='Logo Health Way'></img>
-            <h1>{titulo}</h1>
-        </header>
-    );
+  return (
+    <header className={style.Cabecalho}>
+      <img src={Logo} alt='Logo Health Way' />
+      <h1>{titulo}</h1>
+    </header>
+  );
 };
 
-export{Cabecalho};
+export { Cabecalho };
